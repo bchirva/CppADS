0fadc8063c5fe47f65b51eef7501c55d4df40fb0
b_chirva <bchirva@gmail.com>
2019-05-05 00:15:08 +0500

    Base interface comments

--- src/Container.hpp
+++ src/Container.hpp
@@ -4,10 +4,16 @@
 
 namespace CppADS
 {
+    /// @brief Base class for all containers
     class IContainer
     {
     public:
+
+        /// @brief Remove all data from container
         virtual void clear() = 0;
+
+        /// @brief Get size of container 
+        /// @return Count of items 
         virtual size_t size() const = 0;
     };
 }
